8fe774d9173f2c5a843887b6af88271039ce466e
sadiaimran <sadiaimranmalik24@gmail.com>
2025-01-26 17:19:57 +0500

    done

---
+++ 3assign/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+// Functional Component with TailwindCSS classes
+const Navbar = () => {
+    return (
+      // Main Container using TailwindCSS classes
+      <div>
+        <div id='Hero'className='min-h-screen bg-no-repeat bg-[url(/sadiaimran.png)] bg-right lg:bg-[90%] bg-cover' 
+          style={{backgroundSize: "25%"}}   // Inline style for background size adjustment
+        >
+          <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
+            <div className='flex justify-center text-8xl'>
+              <div>
+                {/* New lines and text display */}
+                <br />
+                <p>I&apos;m </p>
+                <p>Sadia</p>
+                <p>Imran</p>
+              </div>
+            </div>
+            <div className='hidden lg:block'></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Navbar
+  
